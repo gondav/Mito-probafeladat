c0d145eba9e1db43edfefac35f7a4714d7d408c7
gondav <viktoria.gonda@gmail.com>
2024-03-19 20:09:42 +0100

    add Headline component

---
+++ src/components/booking/headline/Headline.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import IconPlane from '@/components/icons/IconPlane.vue';
+</script>
+
+<template>
+  <section>
+    <IconPlane />
+    <h1>Select Flight</h1>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  section {
+    display: flex;
+    align-items: center;
+
+    h1 {
+      margin-left: 24px;
+      color: rgba(10, 0, 139, 1);
+      font-size: 40px;
+      font-weight: 300;
+      text-transform: uppercase;
+    }
+  }
+</style>
