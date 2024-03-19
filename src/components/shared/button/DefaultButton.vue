0affd8f757e1b4ab257a9e665e84c565bc136a42
gondav <viktoria.gonda@gmail.com>
2024-03-19 17:50:31 +0100

    add DefaultButton

---
+++ src/components/shared/button/DefaultButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  interface DefaultButtonProps {
+    label: string
+  }
+
+  defineProps<DefaultButtonProps>()
+</script>
+
+<template>
+  <button>{{ label }}</button>
+</template>
+
+<style scoped lang="scss">
+  button {
+    width: 250px;
+    height: 50px;
+    border: none;
+    color: rgba(255, 255, 255, 1);
+    font-size: 16px;
+    text-transform: uppercase;
+    cursor: pointer;
+    background-color: $color-secondary;
+  }
+</style>
