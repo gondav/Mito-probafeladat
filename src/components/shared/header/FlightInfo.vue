ce9faa2f6e22b73f293919e8a19fae19bcccebd8
gondav <viktoria.gonda@gmail.com>
2024-03-19 21:55:31 +0100

    add FlightInfo component

---
+++ src/components/shared/header/FlightInfo.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+  import IconArrowRight from '@/components/icons/IconArrowRight.vue';
+  import IconArrowLeft from '@/components/icons/IconArrowLeft.vue';
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="origin">
+      <p class="leaving">Leaving From</p>
+      <p>Budapest</p>
+    </div>
+    <div class="icons">
+      <IconArrowRight />
+      <IconArrowLeft />
+    </div>
+    <div class="destination">
+      <p>Barcelona El Prat</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .wrapper {
+    display: flex;
+
+    .icons {
+      display: flex;
+      flex-direction: column;
+      align-self: center;
+      margin: 0 40px;
+    }
+  }
+  
+  p {
+    color: rgba(255, 255, 255, 1);
+
+    &.leaving {
+      font-size: 10px;
+      line-height: 12px;
+      text-transform: uppercase;
+    }
+  }
+</style>
