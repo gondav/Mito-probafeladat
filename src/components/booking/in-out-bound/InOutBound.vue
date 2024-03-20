8dffeddbfbdb5610cadeb300f5454f8358ad5e8e
gondav <viktoria.gonda@gmail.com>
2024-03-20 19:41:12 +0100

    add InOutBound component

---
+++ src/components/booking/in-out-bound/InOutBound.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import InOutBoundHeader from './header/InOutBoundHeader.vue';
+  import InOutBoundBody from './in-out-bound-body/InOutBoundBody.vue';
+  import DateHeading from './date-heading/DateHeading.vue';
+</script>
+
+<template>
+  <div class="container">
+    <InOutBoundHeader />
+    <DateHeading />
+    <InOutBoundBody />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
