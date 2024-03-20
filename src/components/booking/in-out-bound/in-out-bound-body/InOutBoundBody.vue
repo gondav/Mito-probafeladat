c555b695476c407df96f10a6669b36ba1a04d1bb
gondav <viktoria.gonda@gmail.com>
2024-03-20 18:03:16 +0100

    add InOutBoundBody component

---
+++ src/components/booking/in-out-bound/in-out-bound-body/InOutBoundBody.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import InOutBoundRow from './in-out-bound-row/InOutBoundRow.vue';
+</script>
+
+<template>
+  <section>
+    <InOutBoundRow />
+    <InOutBoundRow />
+    <InOutBoundRow />
+  </section>
+</template>
+
+<style scoped lang="scss"></style>
