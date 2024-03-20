af943f36e62d1b086b64dfba14462e3668425e13
gondav <viktoria.gonda@gmail.com>
2024-03-20 12:50:19 +0100

    add arrow chevrons

---
+++ src/components/icons/IconRightArrowChevron.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g clip-path="url(#clip0_42_976)">
+  <path d="M3.78103 16.7938L4.95279 18L14.2188 9.00029L4.95279 0L3.78103 1.20641L11.8041 9.00029L3.78103 16.7938Z" fill="#C6007E"/>
+  </g>
+  <defs>
+  <clipPath id="clip0_42_976">
+  <rect width="18" height="18" fill="white" transform="matrix(-1 0 0 -1 18 18)"/>
+  </clipPath>
+  </defs>
+  </svg>
+</template>
