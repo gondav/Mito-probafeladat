a06b1f91e40ad39c9915e67eb8e74225e78b7557
gondav <viktoria.gonda@gmail.com>
2024-03-20 18:04:19 +0100

    import InOutBound component to Booking component

--- src/components/booking/Booking.vue
+++ src/components/booking/Booking.vue
@@ -2,6 +2,7 @@
   import Header from '../shared/header/Header.vue';
   import Headline from './headline/Headline.vue';
   import FlightSummary from './flight-summary/FlightSummary.vue';
+  import InOutBound from './in-out-bound/InOutBound.vue';
 </script>
 
 <template>
@@ -9,6 +10,7 @@
   <div class="container">
     <Headline />
     <FlightSummary />
+    <InOutBound />
   </div>
 </template>
 
