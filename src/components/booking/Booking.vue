21f48757d5c5d93b6ac201b5c2150907c53a77e3
gondav <viktoria.gonda@gmail.com>
2024-03-20 08:41:17 +0100

    Booking.vue started

---
+++ src/components/booking/Booking.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import Header from '../shared/header/Header.vue';
+  import Headline from './headline/Headline.vue';
+  import FlightSummary from './flight-summary/FlightSummary.vue';
+</script>
+
+<template>
+  <Header />
+  <div class="container">
+    <Headline />
+    <FlightSummary />
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
