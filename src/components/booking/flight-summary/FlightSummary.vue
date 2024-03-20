7c4fb6f84be68a260dae2a3dd48afc16179d4e3f
gondav <viktoria.gonda@gmail.com>
2024-03-20 08:35:35 +0100

    fix css mistakes and add IconSeparator

--- src/components/booking/flight-summary/FlightSummary.vue
+++ src/components/booking/flight-summary/FlightSummary.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
   import DefaultButton from '@/components/shared/button/DefaultButton.vue';
+  import IconSeparator from '@/components/icons/IconSeparator.vue';
 </script>
 
 <template>
@@ -18,6 +19,7 @@
          <p>Wed 06:02 - 07:35</p>
       </div>
     </section>
+    <IconSeparator />
     <section>
       <div class="date">
         <p class="month">Nov</p>
@@ -28,7 +30,10 @@
          <p>Wed 06:02 - 07:35</p>
       </div>
     </section>
-    <DefaultButton label="Total" variant="default" />
+    <section class="summary">
+      <span>Total</span>
+      <span>$29.98</span>
+    </section>
     <DefaultButton label="Pay Now" variant="accent"/>
   </aside>
 </template>
@@ -38,6 +43,7 @@
     max-width: 230px;
     display: grid;
     row-gap: 15px;
+    border-top: 1px solid rgba(193, 193, 193, 1);
 
     section {
       display: flex;
@@ -62,7 +68,6 @@
   }
 
   section .date {
-    width: 36px;
     border: 2px solid rgba(225, 225, 225, 1);
     border-radius: 2px;
     text-align: center;
@@ -94,6 +99,16 @@
     p {
       font-size: 10px;
     }
+  }
 
+  section.summary {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 50px;
+    background-color: $color-primary;
+    color: rgba(255, 255, 255, 1);
+    text-transform: uppercase;
   }
 </style>
