27efcbe7f575375e6945dd1db8165c16113cca89
gondav <viktoria.gonda@gmail.com>
2024-03-21 14:09:12 +0100

    add effects to default buton

--- src/components/shared/button/DefaultButton.vue
+++ src/components/shared/button/DefaultButton.vue
@@ -22,13 +22,30 @@
     font-size: 16px;
     text-transform: uppercase;
     cursor: pointer;
+    transition: background-color 0.3s ease-in-out;
 
     &.default {
       background-color: $color-secondary;
+
+      &:hover {
+        background-color: lighten($color-secondary, 10%);
+      }
+
+      &:active {
+        background-color: darken($color-secondary, 10%);
+      }
     }
 
     &.accent {
       background-color: $color-accent;
+
+      &:hover {
+        background-color: rgb(215, 77, 165);
+      }
+
+      &:active {
+        background-color: rgb(166, 0, 106);
+      }
     }
 
   }
