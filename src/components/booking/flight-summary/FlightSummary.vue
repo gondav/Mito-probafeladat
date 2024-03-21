2d0d793952916cd9803f2f6692f46fa10f0a4f03
gondav <viktoria.gonda@gmail.com>
2024-03-21 13:28:27 +0100

    make scss more structured in FlightSummary component

--- src/components/booking/flight-summary/FlightSummary.vue
+++ src/components/booking/flight-summary/FlightSummary.vue
@@ -44,71 +44,84 @@
     display: grid;
     row-gap: 15px;
     border-top: 1px solid rgba(193, 193, 193, 1);
-
-    section {
-      display: flex;
-      align-items: center;
-      padding: 0 15px;
-    }
+    background-color: $color-white;
   }
 
-  section.top-summary {
-    justify-content: space-between;
-    text-transform: uppercase;
-    font-size: 14px;
-    margin-top: 16px;
-    
-    p {
-      font-weight: bold;
-    }
-
-    .price {
-      color: $color-primary;
-    }
-  }
-
-  section .date {
-    border: 2px solid rgba(225, 225, 225, 1);
-    border-radius: 2px;
-    text-align: center;
-    margin-right: 12px;
-
-    p {
-      padding: 5px;
-    }
-
-    p.month {
-      font-size: 13px;
-    }
-
-    p.day {
-      font-size: 18px;
-    }
-
-    p:first-child {
-      background-color: rgba(242, 242, 242, 1);
-      text-transform: uppercase;
-    }
-  }
-
-  section .destination {
-    h4 {
-      font-size: 14px;
-    }
-
-    p {
-      font-size: 10px;
-    }
-  }
-
-  section.summary {
+  section {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    height: 50px;
-    background-color: $color-primary;
-    color: rgba(255, 255, 255, 1);
-    text-transform: uppercase;
+    padding: 0 15px;
+
+    &.top-summary {
+      justify-content: space-between;
+      text-transform: uppercase;
+      font-size: 14px;
+      margin-top: 16px;
+      
+      p {
+        font-weight: bold;
+
+        &:first-child {
+          color: $color-font-dark;
+        }
+      }
+
+      .price {
+        color: $color-primary;
+      }
+    }
+
+    &.summary {
+      justify-content: space-between;
+      width: 100%;
+      height: 50px;
+      background-color: $color-primary;
+      color: $color-white;
+      text-transform: uppercase;
+    }
+
+    .date {
+      border: 2px solid rgba(225, 225, 225, 1);
+      border-radius: 2px;
+      text-align: center;
+      margin-right: 12px;
+
+      p {
+        
+        font-weight: 600;
+        color: $color-font-dark;
+
+        &.month {
+          padding: 5px;
+          font-size: 13px;
+        }
+
+        &.day {
+          padding: 3px;
+          font-size: 18px;
+        }
+
+        &:first-child {
+          background-color: rgba(242, 242, 242, 1);
+          text-transform: uppercase;
+        }
+      }
+    }
+
+    .destination {
+      h4 {
+        font-size: 14px;
+        margin-bottom: 6px;
+      }
+
+      p {
+        font-size: 10px;
+      }
+
+      h4, p {
+        color: $color-font-dark;
+        font-weight: 600;
+      }
+    }
   }
 </style>
